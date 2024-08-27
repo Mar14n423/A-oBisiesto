1efa9a230eaa289375e10e2d055fc63b5676c2b0
Mar14n423 <wara.mercado@ucb.edu.bo>
2024-08-27 00:10:18 -0400

    año no bisiesto

--- src/Bisiesto.js
+++ src/Bisiesto.js
@@ -1,7 +1,8 @@
 function generarBisiesto(numero){
+  if(numero % 100 === 0 && numero % 400 !== 0)
+    return false;
   if(numero % 400 === 0)
     return true;
-
 }
 
 export default generarBisiesto;
--- src/Bisiesto.spec.js
+++ src/Bisiesto.spec.js
@@ -7,5 +7,8 @@ describe("Bisiesto", () => {
   it("Deberia generar TRUE para cuqluier año divisible entre 400", () => {
     expect(generarBisiesto(2400)).toEqual(true);
   });
+  it("Deberia generar FALSE para cualquier año divisible entre 100 pero no entre 400 como el 2200", () => {
+    expect(generarBisiesto(2200)).toEqual(false);
+  });
 
 });
