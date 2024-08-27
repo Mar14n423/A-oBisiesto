8527e23441a9feb15f9e33491a38ccdf84960e4e
Mar14n423 <wara.mercado@ucb.edu.bo>
2024-08-27 00:18:11 -0400

    año divisble entre 4 pero no entre 100 y sea bisiesto

--- src/Bisiesto.js
+++ src/Bisiesto.js
@@ -1,8 +1,12 @@
 function generarBisiesto(numero){
+  if(numero % 4 === 0 && numero % 100 !== 0)
+    return true;
   if(numero % 100 === 0 && numero % 400 !== 0)
     return false;
   if(numero % 400 === 0)
     return true;
+  if(numero % 4 !== 0)
+    return false;
 }
 
 export default generarBisiesto;
--- src/Bisiesto.spec.js
+++ src/Bisiesto.spec.js
@@ -10,5 +10,17 @@ describe("Bisiesto", () => {
   it("Deberia generar FALSE para cualquier año divisible entre 100 pero no entre 400 como el 2200", () => {
     expect(generarBisiesto(2200)).toEqual(false);
   });
+  it("Deberia generar FALSE para cualquier año divisible entre 100 pero no entre 400", () => {
+    expect(generarBisiesto(2300)).toEqual(false);
+  });
+
+  it("Deberia generar TRUE para un año divisible entre 4 pero no entre 100", () => {
+    expect(generarBisiesto(2008)).toEqual(true);
+  });
+
+  it("Deberia generar TRUE para cualquier año divisible entre 4 pero no entre 100", () => {
+    expect(generarBisiesto(2012)).toEqual(true);
+  });
+  
 
 });
